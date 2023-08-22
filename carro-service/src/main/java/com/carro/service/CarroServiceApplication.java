5f114ee126be06689d7c6b2c8184ac483139b2b6
Nicolás Chicaiza <jefryn@unicauca.edu.co>
2023-08-21 23:11:17 -0500

    config eureka

--- carro-service/src/main/java/com/carro/service/CarroServiceApplication.java
+++ carro-service/src/main/java/com/carro/service/CarroServiceApplication.java
@@ -2,9 +2,10 @@ package com.carro.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class CarroServiceApplication {
 
 	public static void main(String[] args) {
